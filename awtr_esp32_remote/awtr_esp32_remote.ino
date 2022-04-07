0352577311c6540dca006571dc9d513054f45a01
Francesco Zimbolo <francesco@haloghen.it>
2022-04-07 17:24:34 +0200

    Remote Changes

--- awtr_esp32_remote/awtr_esp32_remote.ino
+++ awtr_esp32_remote/awtr_esp32_remote.ino
@@ -5,7 +5,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define CUSTOM_PIN_LAYOUT
+//#define CUSTOM_PIN_LAYOUT
 
 //datas that will be sent to the receiver
 typedef struct {
@@ -29,12 +29,13 @@ const uint64_t WRITE_ADDR_GRAB = 0x47E197009488;
 const uint64_t WRITE_ADDR_FLIP = 0xF71993500B07;
 const uint64_t WRITE_ADDR_WEDG = 0xe197c02702;
 uint64_t current_addr = WRITE_ADDR_GRAB;
-
+int rf_channel =76;
 
 //---------------------------------------ESP_NOW Variables
 //MAC robot hinge  C8:C9:A3:CB:33:F8
 //MAC robot grab    C8:C9:A3:CB:70:54
-uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0xCB, 0x70, 0x54};
+uint8_t hingeAddress[] = {0xC8, 0xC9, 0xA3, 0xCB, 0x33, 0xF8};
+uint8_t egrabAddress[] = {0xC8, 0xC9, 0xA3, 0xCB, 0x70, 0x54};
 String success;
 
 esp_now_peer_info_t peerInfo;
@@ -112,30 +113,29 @@ const int leverPot = 34;
 const int modePot = 39;
 
 const int rightBtn = 0;
-const int leftBtn = 2;
+const int leftBtn = 12;
 const int topBtn = 4;
 const int lowSwitch = 32;
 const int topSwitch = 25;
-const int ledPin = 12;
+const int ledPin = 2;
 
-const int potStrRightEnd = 10; //default 0, reversed 1024
-const int potStrLeftEnd = 4085; //default 1024, reversed 0
+const int potStrRightEnd = 3; //default 0, reversed 1024
+const int potStrLeftEnd = 1020; //default 1024, reversed 0
 
-const int potAccForwardEnd = 4085; // default 0, reversed 1024
-const int potAccBackEnd = 10; //default 1024, reversed 0
+const int potAccForwardEnd = 1020; // default 0, reversed 1024
+const int potAccBackEnd = 3; //default 1024, reversed 0
 
+const int potStrRightStart = 486; //default 512
+const int potStrLeftStart = 494; //default 512
 
-const int potStrRightStart = 1750; //default 512
-const int potStrLeftStart = 1810; //default 512
+const int potAccForwardStart = 476; //default 512
+const int potAccBackStart = 470; //default 512
 
-const int potAccForwardStart = 1835; //default 512
-const int potAccBackStart = 1735; //default 512
+const int potLevUpStart = 465; //default 512
+const int potLevDownStart = 485; //default 512
 
-const int potLevUpStart = 1790; //default 512
-const int potLevDownStart = 1900; //default 512
-
-const int potLevUpEnd = 1030; //default 0, reversed 1024
-const int potLevDownEnd = 2500; //default 1024, reversed 0
+const int potLevUpEnd = 280; //default 0, reversed 1024
+const int potLevDownEnd = 650; //default 1024, reversed 0
 #endif
 
 
@@ -144,7 +144,6 @@ int PWMmax = 255;
 int expoMax = 190; //exp will be this/100
 int analogReadMax = 1023;
 int analogRes = 10;
-int wpn_range = 1023;
 
 //var that will be stored and retreived from
 //eprom mem
@@ -175,11 +174,13 @@ enum bot_control {
   GRAB,
   FLIP,
   WEDG,
-  ESPREC
+  HING,
+  EGRAB
 };
 
-int bot_current = ESPREC;
+int current_bot = EGRAB;
 bool wpnSafetyCeck = true;
+bool wifi_remote = false;
 
 void setup() {
   //store_values(); // uncomment only to initialize mem
@@ -190,48 +191,123 @@ void setup() {
   pinMode(lowSwitch, INPUT_PULLUP);
   pinMode(topSwitch, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin,HIGH);
+  digitalWrite(ledPin, HIGH);
 
   Serial.begin(115200);
 
   //load_values()
 
-  Serial.println("mem READ");
-  Serial.print("angle_act: ");
-  Serial.print(activeAngle);
-  Serial.print(" -- angle_rest: ");
-  Serial.print(restAngle);
-  Serial.print(" -- wpn_acc: ");
-  Serial.print(wpnAccel);
-  Serial.print(" -- str_exp: ");
-  Serial.print(strExpoalpha);
-  Serial.print(" -- acc_exp: ");
-  Serial.println(accExpoalpha);
-  sentData.weaponStrenght = 0;
+
+
 
   //TODO robot select on initalization
-
+  int robot_n = analogRead(modePot)/ 205;
+  //Serial.println(mode_n);
+  
+  switch (robot_n) {
+    case 0: //hinge esp
+    Serial.println("HINGE BOT");
+      wifi_remote = true;
+      current_bot = HING;
+      activeAngle = 100;
+      restAngle = 33;
+      break;
+    case 1: //grab esp
+    Serial.println("FLIPPER BOT");
+      current_bot = FLIP;
+      rev_str = true;
+      current_addr = WRITE_ADDR_FLIP;
+      rf_channel = 85;
+      break;
+     
+      break;
+    case 2: //flipper
+    Serial.println("WEDGE BOT");
+      current_bot = WEDG;
+      rev_str = false;
+      current_addr = WRITE_ADDR_WEDG;
+      activeAngle = 180;
+      restAngle = 83;
+      break;
+     
+    case 3: // wedge bot
+     Serial.println("EGRAB BOT");
+      wifi_remote = true;
+      current_bot = EGRAB;
+      activeAngle = 1023;
+      restAngle = 435;
+      break;
+    case 4://grab ardu
+    
+      Serial.println("ARDU GRAB BOT");
+      current_bot = GRAB;
+      current_addr = WRITE_ADDR_GRAB;
+      break;
+    default:
+      break;
+  }
   //---------------------------------------RF24 Setup
   //radio.begin();
   //radio.setPALevel(RF24_PA_LOW);
   //radio.openWritingPipe(current_addr);
 
   //---------------------------------------ESP NOW setup
-  WiFi.mode(WIFI_STA);
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+  if (wifi_remote) {
+    WiFi.mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK) {
+      Serial.println("Error initializing ESP-NOW");
+      return;
+    }
+    esp_now_register_send_cb(OnDataSent);
+    Serial.println(current_bot== HING);
+    if(current_bot== HING){
+      memcpy(peerInfo.peer_addr, hingeAddress, 6);
+    }else{
+      memcpy(peerInfo.peer_addr, egrabAddress, 6);
+    }
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+      Serial.println("Failed to add peer");
+      return;
+    }
+    esp_now_register_recv_cb(OnDataRecv);
+  } else {
+    radio.begin();
+    radio.setPALevel(RF24_PA_LOW);
+    radio.openWritingPipe(current_addr);
+    radio.setChannel(rf_channel);
   }
-  esp_now_register_send_cb(OnDataSent);
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    return;
-  }
-  esp_now_register_recv_cb(OnDataRecv);
-  
+  /*
+    if (radioSet) {
+    //Serial.print("serring radio...");
+    //Serial.println(current_bot);
+    if (modeValue < 300 && current_bot != GRAB) {
+      current_bot = GRAB;
+      rev_str = false;
+      current_addr = WRITE_ADDR_GRAB;
+      radio.setChannel(76);
+      radio.openWritingPipe(current_addr);
+    }
+    if (modeValue > 301 && modeValue < 600 && current_bot != FLIP) {
+      current_bot = FLIP;
+      rev_str = true;
+      current_addr = WRITE_ADDR_FLIP;
+      radio.setChannel(85);
+      radio.openWritingPipe(current_addr);
+
+    }
+    if (modeValue > 601 && current_bot != WEDG) {
+      current_bot = WEDG;
+      rev_str = false;
+      current_addr = WRITE_ADDR_WEDG;
+      activeAngle = 166;
+      restAngle = 74;
+      radio.setChannel(76);
+      radio.openWritingPipe(current_addr);
+
+    }
+    }*/
 }
 
 
@@ -243,10 +319,10 @@ void loop() {
   int templeverValue = analogRead(leverPot);
   modeValue = analogRead(modePot);
 
-  leverValue_f = leverValue*0.8 +  templeverValue*0.2;
+  leverValue_f = leverValue * 0.8 +  templeverValue * 0.2;
   leverValue = (int)leverValue_f;
-  
-  int check_debug=modeValue;
+
+  int check_debug = leverValue;
   if (check_debug > debug_max) {
     debug_max = check_debug ;
   }
@@ -254,17 +330,17 @@ void loop() {
     debug_min = check_debug;
   }
   /*
-  Serial.print(debug_max);
-  Serial.print("\t");
-  Serial.print(debug_min);
-  Serial.print("\t");
-  Serial.println(debug_max-debug_min);
-  */
+    Serial.print(debug_max);
+    Serial.print("\t");
+    Serial.print(debug_min);
+    Serial.print("\t");
+    Serial.println(debug_max-debug_min);
+  //*/
   //read btns values
-  bool setMode = !digitalRead(lowSwitch);
+  bool setMode = false;//!digitalRead(lowSwitch);
 
 
-  bool storeValue = !digitalRead(topSwitch);
+  bool storeValue = false;//!digitalRead(topSwitch);
   bool radioSet = false;//!digitalRead(topSwitch);
 
   bool rightValue = !digitalRead(rightBtn);
@@ -278,44 +354,25 @@ void loop() {
   bool wpnSet = false;
   bool mode5Set = false;
 
-  int mode_n = modeValue / 205;
-  //Serial.println(mode_n);
-  if (setMode) {
-    switch (mode_n) {
-      case 0:
-        drSet = true;
-        break;
-      case 1:
-        expoSet = true;
-        break;
-      case 2:
-        safetySet = true;
-        break;
-      case 3:
-        wpnSet = true;
-        break;
-      case 4:
-        mode5Set = true;
-        break;
-      default:
-        break;
-    }
-  }
+
 
   //----------------------------------------------------WEAPON CODE
 
   if (rightValue) {
-    sentData.weaponArg = 100;
+    sentData.weaponArg = activeAngle;
     sentData.Fire = true;
   } else {
-    wpn = map(leverValue, potLevUpEnd, potLevDownEnd, 0, wpn_range);
-  wpn = constrain(wpn, 0, wpn_range);
-  sentData.weaponArg = wpn;
+    wpn = map(leverValue, potLevUpEnd, potLevDownEnd, restAngle, activeAngle);
+    wpn = constrain(wpn, restAngle, activeAngle);
+    sentData.weaponArg = wpn;
+  }
+  if(topValue){
+    sentData.weaponArg = restAngle;
   }
 
   sentData.weaponStrenght = PWMmax;
-  sentData.weaponAccel = 0;
-  
+  sentData.weaponAccel = 8;
+
   if (safetySet) {
     //FIX MSSING POT
     /*
@@ -333,37 +390,8 @@ void loop() {
   }
 
 
-  if (radioSet) {
-    //Serial.print("serring radio...");
-    //Serial.println(bot_current);
-    if (modeValue < 300 && bot_current != GRAB) {
-      bot_current = GRAB;
-      rev_str = false;
-      current_addr = WRITE_ADDR_GRAB;
-      radio.setChannel(76);
-      radio.openWritingPipe(current_addr);
-    }
-    if (modeValue > 301 && modeValue < 600 && bot_current != FLIP) {
-      bot_current = FLIP;
-      rev_str = true;
-      current_addr = WRITE_ADDR_FLIP;
-      radio.setChannel(85);
-      radio.openWritingPipe(current_addr);
 
-    }
-    if (modeValue > 601 && bot_current != WEDG) {
-      bot_current = WEDG;
-      rev_str = false;
-      current_addr = WRITE_ADDR_WEDG;
-      activeAngle = 166;
-      restAngle = 74;
-      radio.setChannel(76);
-      radio.openWritingPipe(current_addr);
 
-    }
-  }
-
-  
 
 
   //----------------------------------------------------MOTOR CODE
@@ -441,7 +469,7 @@ void loop() {
   //recorrect the data not to have more than max +PWM while non pivot-steering
   sentData.speedmotorLeft = left_pwm;
   sentData.speedmotorRight = right_pwm;
-  
+
 
 
   if (!storeValue) {
@@ -453,17 +481,21 @@ void loop() {
     }
   }
 
-  if (bot_current != ESPREC) {
+  if (!wifi_remote) {
     if (!radio.write( &sentData, sizeof(sentData) )) {
-      //Serial.println(F("failed"));
+      Serial.println(F("failed"));
     }
   } else {
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &sentData, sizeof(sentData));
-
+    esp_err_t result = -1;
+    if(current_bot == EGRAB){
+      result = esp_now_send(egrabAddress, (uint8_t *) &sentData, sizeof(sentData));
+    }else{
+      result = esp_now_send(hingeAddress, (uint8_t *) &sentData, sizeof(sentData));
+     }
     if (result == ESP_OK) {
       //Serial.println("Sent with success");
     } else {
-      //Serial.println("Error sending the data");
+      Serial.println("Error sending the data");
     }
   }
 
@@ -482,42 +514,42 @@ void loop() {
     //*/
 
   /*DEBUG INPUT
-  Serial.print("accpot: ");
-  Serial.print(accValue);
-  Serial.print("\t");
+    Serial.print("accpot: ");
+    Serial.print(accValue);
+    Serial.print("\t");
 
-  Serial.print("strpot");
-  Serial.print(strValue);
-  Serial.print("\t");
+    Serial.print("strpot");
+    Serial.print(strValue);
+    Serial.print("\t");
 
-  Serial.print("leverpot");
-  Serial.print(leverValue);
-  Serial.print("\t");
-  
-  Serial.print("BL: ");
-  Serial.print(leftValue);
-  Serial.print("\t");
-  
-  Serial.print("BR: ");
-  Serial.print(rightValue);
-  Serial.print("\t");
+    Serial.print("leverpot");
+    Serial.print(leverValue);
+    Serial.print("\t");
 
-  Serial.print("BT: ");
-  Serial.print(topValue);
-  Serial.print("\t");
+    Serial.print("BL: ");
+    Serial.print(leftValue);
+    Serial.print("\t");
+
+    Serial.print("BR: ");
+    Serial.print(rightValue);
+    Serial.print("\t");
+
+    Serial.print("BT: ");
+    Serial.print(topValue);
+    Serial.print("\t");
 
 
-  Serial.print("TSW: ");
-  Serial.print(storeValue);
-  Serial.print("\t");
+    Serial.print("TSW: ");
+    Serial.print(storeValue);
+    Serial.print("\t");
 
-  Serial.print("BSW: ");
-  Serial.print(setMode);
-  Serial.print("\t");
+    Serial.print("BSW: ");
+    Serial.print(setMode);
+    Serial.print("\t");
 
-  Serial.print("modePot: ");
-  Serial.println(modeValue);
-  //*/
+    Serial.print("modePot: ");
+    Serial.println(modeValue);
+    //*/
 }
 
 void load_values() {
